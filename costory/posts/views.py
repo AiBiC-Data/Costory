ad5161374919510550122650e27a25340885d7e7
kwonho <whrnjsgh12@naver.com>
2021-08-10 16:16:35 +0900

    apply_form,css, finish

--- costory/posts/views.py
+++ costory/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 # Create your views here.
 def post_list(request):
     posts= Post.objects.all()
@@ -10,3 +12,35 @@ def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     context = {"post": post}
     return render(request, 'posts/post_detail.html', context)
+
+def post_create(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            new_post = post_form.save()
+            return redirect('post-detail', post_id=new_post.id)
+    else:
+        post_form = PostForm()
+    return render(request, 'posts/post_form.html', {'form': post_form})
+
+def post_update(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method=='POST':
+        post_form=PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('post-detail', post_id=post.id)
+    else:
+        post_form = PostForm(instance=post)
+    return render(request, 'posts/post_form.html', {'form':post_form})
+
+def post_delete(request, post_id):
+    post=Post.objects.get(id=post_id)
+    if request.method=='POST':
+        post.delete()
+        return redirect('post-list')
+    else:
+        return render(request, 'posts/post_confirm_delete.html', {'post': post})
+
+def index(request):
+    return redirect('post-list')
