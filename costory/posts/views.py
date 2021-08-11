67e97c6b048c1307b470ac3a92e761b6fbb4e65b
kwonho <whrnjsgh12@naver.com>
2021-08-11 14:03:02 +0900

    input seeding, reinforce verification, pagination

--- costory/posts/views.py
+++ costory/posts/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
+from django.core.paginator import Paginator
+
 # Create your views here.
+
+
 def post_list(request):
     posts= Post.objects.all()
-    context = {"posts" : posts}
-    return render(request, 'posts/post_list.html', context)
+    paginator = Paginator(posts, 6)
+    curr_page_number = request.GET.get('page')
+    if curr_page_number is None:
+        curr_page_number = 1
+    page = paginator.page(curr_page_number)
+    return render(request,'posts/post_list.html', {'page':page})
+
 
 def post_detail(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     context = {"post": post}
     return render(request, 'posts/post_detail.html', context)
+
 
 def post_create(request):
     if request.method == 'POST':
@@ -23,8 +32,9 @@ def post_create(request):
         post_form = PostForm()
     return render(request, 'posts/post_form.html', {'form': post_form})
 
+
 def post_update(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     if request.method=='POST':
         post_form=PostForm(request.POST, instance=post)
         if post_form.is_valid():
@@ -34,13 +44,15 @@ def post_update(request, post_id):
         post_form = PostForm(instance=post)
     return render(request, 'posts/post_form.html', {'form':post_form})
 
+
 def post_delete(request, post_id):
-    post=Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     if request.method=='POST':
         post.delete()
         return redirect('post-list')
     else:
         return render(request, 'posts/post_confirm_delete.html', {'post': post})
+
 
 def index(request):
     return redirect('post-list')
